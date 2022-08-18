1b1a6deafbc9075770033ab705106b4093104ce6
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-18 22:23:23 +0500

    js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 function App() {
+
+    const sayHello = () => {
+        console.log("Say Hello-world!")
+    }
+
+    const counter = 0;
     return (
         <div>
             <h1>Hello world</h1>
+            <button onClick={sayHello}>Click 1</button>
+            <button>{counter}</button>
         </div>
     )
 };
