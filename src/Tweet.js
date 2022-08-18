84016a8c3f5c2f05fb090c448e167aede7d27e0c
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-19 00:46:58 +0500

    tweet updated

---
+++ src/Tweet.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Tweet = () => {
+    return (
+        <div>
+            <h1>Name:</h1>
+            <p>This is random Tweet.</p>
+            <h3>Number of likes.</h3>
+        </div>
+    )
+
+};
+
+export default Tweet;
