787914f9d28fdce3b0c32101a83f371e3bd44108
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-19 20:49:43 +0500

    component added

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./App.css";
 
 const Tweet = () => {
     return (
-        <div>
+        <div className="tweet">
             <h1>Name:</h1>
             <p>This is random Tweet.</p>
             <h3>Number of likes.</h3>
