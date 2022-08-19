8164d1d273b8799dd8d0afcedb0d664d032e2cd2
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-19 20:49:05 +0500

    css applied

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Tweet from "./Tweet";
-import "./App.css";
 
 function App() {
     return (
-        <div className="tweet">
-            <Tweet className="app" />
+        <div className="app">
+            <Tweet />
             <Tweet />
             <Tweet />
         </div>
