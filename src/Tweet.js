fceae047e72c1a583d9df203e39bec02f235928b
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-19 21:56:16 +0500

    🗳️component tweet

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 
-const Tweet = () => {
+const Tweet = (props) => {
     return (
         <div className="tweet">
-            <h1>Name:</h1>
-            <p>This is random Tweet.</p>
-            <h3>Number of likes.</h3>
+            <h1>Name: {props.name}</h1>
+            <p>This is random Tweet: {props.name}</p>
+            <h3>Number of poeple:{props.name}</h3>
         </div>
     )
 
