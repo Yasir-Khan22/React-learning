8991fda5377b6f53ba260a99d83cff442564387f
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-26 23:08:12 +0500

    js added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from "react";
 
 const App = () => {
 
-    const APP_ID = "f58c0a52";
-    const APP_KEY = "ada6a423657b9c6bf2d4d33ff0d58a13";
-    const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&pp_key=${APP_KEY}`;
+    // const APP_ID = "f58c0a52";
+    // const APP_KEY = "ada6a423657b9c6bf2d4d33ff0d58a13";
+    // const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&pp_key=${APP_KEY}`;
 
     const [count, setCounter] = useState(0);
 
     useEffect(() => {
         console.log("Hello world!")
-    }, [])
+    })
 
 
     return (
