2d8d6b87a0eaefd308f06b5e2b630f6d623194af
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-26 20:48:26 +0500

    📁app js added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+const App = () => {
+    const APP_ID = "f58c0a52";
+    const API_KEY = "ada6a423657b9c6bf2d4d33ff0d58a13";
 
-function App() {
     return (
-        <div className="app">
-            <h1>Hello React.</h1>
+        <div className="App">
+            <h1>hello react</h1>
         </div>
-    )
-};
-
+    );
+}
 export default App;
