0f71a479353daf080a5c1bd441c11ec38b3c32f0
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-26 21:16:26 +0500

    app js updated.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 const App = () => {
+
     const APP_ID = "f58c0a52";
     const API_KEY = "ada6a423657b9c6bf2d4d33ff0d58a13";
+    const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&pp_key=${API_KEY}`;
+
+    const [count, setCounter] = useState(0);
+
+    useEffect(() => {
+        console.log("Hello world!")
+    })
+
 
     return (
         <div className="App">
-            <h1>hello react</h1>
+            <form className="search-form">
+                <input className="search-bar" type="text" />
+                <button className="search-btn" type="sumbit">Search</button>
+            </form>
         </div>
     );
 }
