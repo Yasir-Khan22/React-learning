d17dda31353513d8be4b62695faafd66d5c8830f
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-26 20:32:59 +0500

    app js added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Tweet from "./Tweet";
 
 function App() {
     return (
         <div className="app">
-            <Tweet name="Yasir" />
-            <Tweet name="Zubair" />
-            <Tweet name="Fasihullah" />
+            <h1>Hello React.</h1>
         </div>
     )
 };
