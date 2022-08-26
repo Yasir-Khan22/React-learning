1bd29a302b7b09f14d728271a647e67c0f7b4d69
Yasir Khan <yasirkhanone22@gmail.com>
2022-08-26 23:27:25 +0500

    js files are added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const App = () => {
 
-    // const APP_ID = "f58c0a52";
-    // const APP_KEY = "ada6a423657b9c6bf2d4d33ff0d58a13";
-    // const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&pp_key=${APP_KEY}`;
+    const APP_ID = "f58c0a52";
+    const APP_KEY = "ada6a423657b9c6bf2d4d33ff0d58a13";
 
-    const [count, setCounter] = useState(0);
+    // useEffect(() => {
+    //     getRecipes();
+    // })
 
-    useEffect(() => {
-        console.log("Hello world!")
-    })
-
+    const getRecipes = async () => {
+        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&pp_key=${APP_KEY}`);
+        const data = await response.json();
+        console.log(data)
+    };
 
     return (
         <div className="App">
             <form className="search-form">
                 <input className="search-bar" type="text" />
-                {/* <button className="search-btn" type="sumbit"></button> */}
-                <h1 onClick={() => setCounter(count + 1)}>{count}</h1>
+                <button className="search-btn" type="sumbit">Search</button>
             </form>
         </div >
     );
