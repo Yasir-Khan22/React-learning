94b97b20f00c0c1425f3c28f508c4f5f74f4e027
Yasir-Khan22 <yasirkhanone22@gmail.com>
2022-09-30 00:57:01 +0500

    file updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React, { useEffect } from "react";
-const App = () => {
 
-    const APP_ID = "f58c0a52";
-    const APP_KEY = "ada6a423657b9c6bf2d4d33ff0d58a13";
+import React from 'react';
 
-    useEffect(() => {
-        getRecipes();
-    })
-
-    const getRecipes = async () => {
-        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&pp_key=${APP_KEY}`);
-        const data = await response.json();
-        console.log(data)
-    };
-
+function App() {
     return (
-        <div className="App">
-            <form className="search-form">
-                <input className="search-bar" type="text" />
-                <button className="search-btn" type="sumbit">Search</button>
-            </form>
-        </div >
-    );
+        <div>App</div>
+    )
 }
-export default App;
+
+export default App
