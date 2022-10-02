b46d3cecca6211364484a0ff256086008972b566
Yasir Khan <yasirkhanone22@gmail.com>
2022-10-02 20:36:12 +0500

    📁js folder updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-
 import React from 'react';
+import { useRef } from 'react';
+import "./App.css";
 
 function App() {
+    const txtTitle = useRef();
+    const hexColor = useRef();
+
+    const Submit = (e) => {
+        e.preventDefault();
+        const title = txtTitle.current.value;
+        const color = hexColor.current.value;
+
+        alert(`${color} ${title}`);
+
+
+    }
     return (
-        <div>App</div>
+        <form onSubmit={Submit}>
+            <input useRef={txtTitle} type="text" placeholder="Enter your color.." />
+            <input useRef={hexColor} type="color" />
+            <button>add</button>
+        </form>
     )
 }
 
